b8972aac334980f042aea669e4f2b86c60d3becb
oibioib <pavelbodal@gmail.com>
2023-06-18 13:25:05 +0200

    feat: implement command line interface subtask

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const output = [];
+
+  for (let i = 0; i < args.length; i += 2) {
+    const arg = args[i].slice(2);
+    const argValue = args[i + 1];
+    output.push(`${arg} is ${argValue}`);
+  }
+
+  console.log(output.join(', '));
 };
 
 parseArgs();
