2b828ea4fcd403b3f377c9682f86c6c3094e132e
oibioib <pavelbodal@gmail.com>
2023-06-18 19:02:28 +0200

    feat: implement streams subtask

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import { createReadStream } from 'fs';
+import { stdout } from 'process';
+import { FS_ERROR_MESSAGE } from '../constants/constants.js';
+
+const fileUrl = new URL('./files/fileToRead.txt', import.meta.url);
+
 const read = async () => {
-    // Write your code here 
+  const stream = createReadStream(fileUrl);
+  stream.pipe(stdout);
+
+  stream.on('error', () => {
+    throw new Error(FS_ERROR_MESSAGE);
+  });
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import { createWriteStream } from 'fs';
+import { stdin } from 'process';
+import { FS_ERROR_MESSAGE } from '../constants/constants.js';
+
+const fileUrl = new URL('./files/fileToWrite.txt', import.meta.url);
+
 const write = async () => {
-    // Write your code here 
+  const stream = createWriteStream(fileUrl);
+  stdin.pipe(stream);
+
+  stream.on('error', () => {
+    throw new Error(FS_ERROR_MESSAGE);
+  });
 };
 
 await write();
