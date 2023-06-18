01c964664b17edf95010d315dbd3b7b0a5b70098
oibioib <pavelbodal@gmail.com>
2023-06-18 14:48:42 +0200

    feat: implement hash subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { readFile } from 'node:fs/promises';
+import { createHash } from 'node:crypto';
+import { FS_ERROR_MESSAGE } from '../constants/constants.js';
+
+const filePath = './files/fileToCalculateHashFor.txt';
+const fileUrl = new URL(filePath, import.meta.url);
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const fileBuffer = await readFile(fileUrl);
+    const hash = createHash('sha256').update(fileBuffer).digest('hex');
+    console.log(`${filePath} hash: `, hash);
+  } catch {
+    throw new Error(FS_ERROR_MESSAGE);
+  }
 };
 
 await calculateHash();
