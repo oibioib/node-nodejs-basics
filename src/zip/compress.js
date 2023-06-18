545393ef81782315e425866ab536086f159db834
oibioib <pavelbodal@gmail.com>
2023-06-18 20:18:42 +0200

    feat: implement zlib subtask

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { ZLIB_ERROR_COMPRESS } from '../constants/constants.js';
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+  try {
+    const fileToCompressUrl = new URL('./files/fileToCompress.txt', import.meta.url);
+    const archiveUrl = new URL('./files/archive.gz', import.meta.url);
+
+    const fileToCompress = createReadStream(fileToCompressUrl);
+    const archive = createWriteStream(archiveUrl);
+    const gzip = createGzip();
+
+    fileToCompress.pipe(gzip).pipe(archive);
+  } catch {
+    throw new Error(ZLIB_ERROR_COMPRESS);
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createUnzip } from 'zlib';
+import { ZLIB_ERROR_DECOMPRESS } from '../constants/constants.js';
+
 const decompress = async () => {
-    // Write your code here 
+  try {
+    const archiveUrl = new URL('./files/archive.gz', import.meta.url);
+    const decompressUrl = new URL('./files/fileToCompress.txt', import.meta.url);
+
+    const archive = createReadStream(archiveUrl);
+    const decompress = createWriteStream(decompressUrl);
+    const unzip = createUnzip();
+
+    archive.pipe(unzip).pipe(decompress);
+  } catch {
+    throw new Error(ZLIB_ERROR_DECOMPRESS);
+  }
 };
 
 await decompress();
